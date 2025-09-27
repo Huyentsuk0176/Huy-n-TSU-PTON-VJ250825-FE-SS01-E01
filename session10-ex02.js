0abcdf95e4f6b1e20554d40073d8e7471c8e5b23
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-09-28 00:23:33 +0900

    session10-ex02

---
+++ session10-ex02.js
@@ -0,0 +1,17 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = "Hà Nội";
+
+document.write("Gia tri cua i:" + i + "<br>");
+document.write("Gia tri cua f:" + f + "<br>");
+document.write("Gia tri cua b:" + b + "<br>");
+document.write("Gia tri cua s:" + s + "<br>");
+
+
+
+
+
+
+
+
