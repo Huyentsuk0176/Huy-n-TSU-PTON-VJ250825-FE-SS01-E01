14ee7c7700f19f0ea504d6cd2a479587c95364f4
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-09-28 23:14:36 +0900

    session10-ex04

---
+++ session10-ex04.js
@@ -0,0 +1,22 @@
+// Nhập 2 số từ bàn phím
+let firstNumber = parseFloat(prompt("Nhập số thứ nhất:"));
+let secondNumber = parseFloat(prompt("Nhập số thứ hai:"));
+
+// Tính tổng
+let sum = firstNumber + secondNumber;
+
+// Hiển thị kết quả
+alert("Tổng của " + firstNumber + " và " + secondNumber + " là: " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
