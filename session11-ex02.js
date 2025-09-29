83cf56c67d1232f6e9865b8c944bf4e290fac81a
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-09-29 18:00:35 +0900

    session11-ex02

---
+++ session11-ex02.js
@@ -0,0 +1,20 @@
+
+
+let answer = prompt("What is the 'official' name of JavaScript?");
+if (answer === "ECMAScript") {
+    alert("Right!");   
+}
+else {
+    alert("Did't know? ECMAScript!");
+}
+
+
+
+
+
+
+
+
+
+
+
