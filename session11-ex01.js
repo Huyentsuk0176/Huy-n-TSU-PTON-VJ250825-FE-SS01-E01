1d6e55610067f04104e46a0c9bf459b77b333c46
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-09-29 17:32:23 +0900

    session11-ex01

---
+++ session11-ex01.js
@@ -0,0 +1,16 @@
+let year = Number(prompt('Nhập năm vào muốn kiểm tra*: '));
+if (year % 4 === 0 && year % 100 !== 0) {
+  alert(`${year} là năm nhuận`);
+} else {
+  alert(`${year} không phải là năm nhuận`);
+}
+
+
+
+
+
+
+
+
+
+
