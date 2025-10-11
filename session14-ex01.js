d5feff0f8d0b193ae3b90346b758484912863736
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-10-11 21:45:18 +0900

    session14-ex01

---
+++ session14-ex01.js
@@ -0,0 +1,16 @@
+let me = {
+  name: "Pham Huyen",
+  job: "Baker",
+  studying: "IT & Japanese"
+};
+console.log(me.name);  // In ra: Pham Huyen
+console.log(me);       // In ra toàn bộ object
+
+  
+
+
+
+
+
+
+
