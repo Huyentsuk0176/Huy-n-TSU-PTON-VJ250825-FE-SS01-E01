86f2701727b2bebdfd9bd350892b45ad92e32ba9
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-10-11 22:02:47 +0900

    session14-ex02

---
+++ session14-ex02.js
@@ -0,0 +1,18 @@
+//Thêm và sửa thuộc tính của đối tượng car
+//Tạo một đối tượng car với các thuộc tính sau:
+
+//brand: "Toyota"
+//model: "Corolla"
+//year: 2020
+//Thêm một thuộc tính mới là color với giá trị "red"
+//Cập nhật giá trị của thuộc tính year thành 2022
+//In ra đối tượng sau khi cập nhật
+ 
+let car = {
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car.color = "red"; // Thêm thuộc tính color
+car.year = 2022; // Cập nhật thuộc tính year
+console.log(car); // In ra đối tượng car sau khi cập nhật
