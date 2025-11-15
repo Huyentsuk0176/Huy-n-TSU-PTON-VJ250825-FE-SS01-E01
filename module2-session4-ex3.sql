15ae787e5785e152b0d1713ed5c1470df88fa738
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-11-15 22:08:57 +0900

    module2-session4-ex3.sql

---
+++ module2-session4-ex3.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Students (
+    StudentID INT,
+    Name VARCHAR(50),
+    Age INT,
+    Major VARCHAR(50)
+);
+INSERT INTO Students (StudentID, Name, Age, Major)
+VALUES
+    (1, 'Alice', 20, 'Computer Science'),
+    (2, 'Bob', 22, 'Mathematics'),
+    (3, 'Charlie', 21, 'Physics');
+    DELETE FROM Students
+    WHERE StudentID = 1
+    LIMIT 1;
+    
+    SELECT * FROM Students;
+    
