04f0f6f29e69d1758b3a3196842a9c88b337111b
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-11-17 19:49:19 +0900

    module2-session5-ex4.sql

--- module2-session5-ex4.sql
+++ module2-session5-ex4.sql
@@ -1,29 +1,18 @@
-CREATE DATABASE EmployeeSalariesDB;
-USE  EmployeeSalariesDB; 
-CREATE TABLE employeeSalaries (
-    employeeID INT PRIMARY KEY,
-    departmentID INT NOT NULL,
-    salary DECIMAL(10,2) NOT NULL
-);
-INSERT INTO employeeSalaries (employeeID, departmentID, salary) VALUES
-(1, 1, 5000000),
-(2, 1, 6000000),
-(3, 1, 5500000),
+CREATE DATABASE ProductsDB;
+USE ProductsDB;
+INSERT INTO products (productID, productName, price) VALUES
+(1, 'Bánh Mì', 15000),
+(2, 'Sữa Tươi', 22000),
+(3, 'Kẹo Socola', 45000),
+(4, 'Mì Ly', 12000),
+(5, 'Nước Ngọt', 18000),
+(6, 'Khoai Tây Chiên', 35000);
+SELECT *
+FROM products
+WHERE price = (SELECT MAX(price) FROM products);
 
-(4, 2, 4500000),
-(5, 2, 4700000),
-
-(6, 3, 7000000),
-(7, 3, 7200000),
-(8, 3, 6800000);
----Truy vấn tính tổng lương + lương trung bình theo phòng ban
-SELECT
-departmentID,
-SUM(salary) AS totalSalary,
-AVG(salary) AS avergeSalary
-FROM employeeSalaries
-GROUP BY departmentID;
-
-
+SELECT *
+FROM products
+WHERE price = (SELECT MIN(price) FROM products);
 
 
