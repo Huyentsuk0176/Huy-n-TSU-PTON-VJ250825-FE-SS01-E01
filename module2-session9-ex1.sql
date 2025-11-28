4522ed8666a10811d4822806792d169109543254
Huyentsuk0176 <Huyentsuk0176@gmail.com>
2025-11-28 14:33:10 +0900

    module2-session9-ex1.sql

--- module2-session9-ex1.sql
+++ module2-session9-ex1.sql
@@ -1,78 +1,105 @@
+CREATE DATABASE Session9_ex1;
+USE Session9_ex1;
 
-CREATE DATABASE bank_view;
-USE bank_view;
 CREATE TABLE Branch (
-    BranchID INT PRIMARY KEY AUTO_INCREMENT,
-    BranchName VARCHAR(100),
-    Location VARCHAR(255),
-    PhoneNumber VARCHAR(15)
+BranchID INT PRIMARY KEY AUTO_INCREMENT,
+BranchName VARCHAR(100)NOT NULL,
+Location VARCHAR(100),
+PhoneNumber VARCHAR(20)
 );
-INSERT INTO Branch (BranchName, Location, PhoneNumber) VALUES
-('Chi nhánh Hà Nội', '123 Trần Hưng Đạo', '0241234567'),
-('Chi nhánh TP.HCM', '456 Lê Lợi', '0287654321'),
-('Chi nhánh Đà Nẵng', '789 Nguyễn Văn Linh', '0236123456');
 CREATE TABLE Employees (
-    EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
-    FullName VARCHAR(100),
-    Position VARCHAR(50),
-    Salary DECIMAL(15,2),
-    HireDate DATE,
-    PhoneNumber VARCHAR(15),
-    BranchID INT,
-    FOREIGN KEY (BranchID) REFERENCES Branch(BranchID)
+EmployeeID INT PRIMARY KEY AUTO_INCREMENT,
+FullName VARCHAR(100)NOT NULL,
+Position VARCHAR(50),
+Salary DECIMAL(15,0)NOT NULL,
+HireDate DATE,
+PhoneNumber VARCHAR(20),
+BranchID INT,
+CONSTRAINT fk_employees_branch
+FOREIGN KEY(BranchID)REFERENCES Branch(BranchID)
 );
-INSERT INTO Employees (FullName, Position, Salary, HireDate, PhoneNumber, BranchID) VALUES
-('Nguyễn Văn An', 'Giám đốc', 45000000, '2018-03-10', '0911111111', 1),
-('Phạm Hoàng Kiên', 'Sale', 18000000, '2023-05-12', '0922222222', 1),
-('Trần Thị Hạnh', 'Giao dịch viên', 15000000, '2021-06-20', '0933333333', 2),
-('Đặng Hữu Bình', 'Quản lý', 32000000, '2023-06-12', '0944444444', 2),
-('Lê Minh Tuấn', 'Kế toán', 10000000, '2022-01-05', '0955555555', 3);
-SELECT * FROM Branch;
-SELECT * FROM Employees;
+DESC Employees;
+INSERT INTO Branch(BranchName, Location, PhoneNumber)
+VALUES
+('Chi nhanh Ha Noi', 'Ha Noi', '024-111-222'),
+('Chi nhanh Ho Chi Minh', 'Ho Chi Minh', '028-333-444'),
+('Chi nhanh Da Nang', 'Da Nang', '0236-555-666');
+INSERT INTO Employees(FullName, Position, Salary, HireDate, PhoneNumber, BranchID)
+VALUES
+('Nguyen Van A', 'Nhan Vien Ban Hang', 12000000,'2022-01-10','0901-111-111',1),
+('Tran Thi B', 'Quan Ly', 20000000,'2021-05-15','0902-222-222',1),
+('Le Van C', 'Nhan Vien Kho',15000000,'2023-03-20','0903-333-333',2),
+('Pham Thi D','Ke Toan',18000000,'2020-07-01','0604-444-444',2),
+('Hoang van E','Nhan Vien Ban Hang',9000000, '2024-01-05','0905-555-555',1);
+
 CREATE VIEW EmployeeBranch AS
-SELECT 
-    e.EmployeeID,
-    e.FullName AS EmployeeName,
-    e.Position,
-    e.Salary,
-    e.HireDate,
-    e.PhoneNumber AS EmployeePhone,
-    b.BranchName,
-    b.Location,
-    b.PhoneNumber AS BranchPhone
-FROM Employees e
-JOIN Branch b
-ON e.BranchID = b.BranchID;
-CREATE VIEW HighSalaryEmployees AS
-SELECT *
-FROM Employees
-WHERE Salary >= 15000000
-WITH CHECK OPTION;
-SELECT * FROM EmployeeBranch;
-SELECT * FROM HighSalaryEmployees;
-
-DELETE e
-FROM Employees e
-JOIN Branch b
-ON e.BranchID = b.BranchID
-WHERE b.BranchName = 'Chi nhánh Hà Nội';
-
-SELECT * 
-FROM Employees e
-JOIN Branch b
-ON e.BranchID = b.BranchID;
-
-
-
-
-
-
-
-
+SELECT
+   e.EmployeeID,
+   e.FullName,
+   e.Position,
+   e.Salary,
+   e.HireDate,
+   b.BranchName,
+   b.Location
+   FROM Employees e
+   JOIN Branch b
+   ON e.BranchID = b.BranchID;
+   SELECT database();
+   SHOW TABLES;
+   SHOW FULL TABLES WHERE Table_type = 'VIEW';
+   SELECT * FROM Employees;
+   SELECT * FROM Branch;
+   select * from EmployeeBranch;
+   
+   DROP VIEW IF EXISTS HighSalaryEmployees;
+   CREATE VIEW HighSalaryEmployees AS
+   SELECT
+       EmployeeID,
+       FullName,
+       Position,
+       Salary,
+       HireDate,
+       PhoneNumber,
+       BranchID
+	FROM employees
+    WHERE Salary >= 15000000
+    WITH CHECK OPTION;
+    
+   INSERT INTO HighSalaryEmployees(FullName, Position, Salary, HireDate, PhoneNumber, BranchID) VALUES
+   ('Pham Thi Huyen', 'Nhom Truong', 20000000, '2025-11-01', '0709-225-383',1);
+   
+   SHOW FULL TABLES WHERE Table_type = 'VIEW';
+   SELECT * FROM EmployeeBranch;
+   SELECT * FROM HighSalaryEmployees;
+   
+   ALTER VIEW EmployeeBranch AS
+   SELECT
+   e.EmployeeID, e.FullName, e.Position, e.Salary, e. HireDate, e.PhoneNumber AS EmployeePhone,
+   b.BranchName,
+   b.Location,
+   b.PhoneNumber AS BranchPhone
+   FROM Employees e
+   JOIN Branch b
+   ON e.BranchID=b.BranchID;
+   SELECT * FROM EmployeeBranch;
+   
+   --xoa du lieu nhan vien thuoc chi nhanh ha noi
+   DELETE e 
+   FROM Employees e
+   JOIN Branch b
+   ON e.BranchID = b.BranchID
+   WHERE b.BranchName='Chi nhanh Ha Noi';
+   select * from Employees;
+   select * from EmployeeBranch;
    
    
    
-
+   
+   
+   
+   
+   
+   
 
 
 
